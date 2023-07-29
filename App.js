92696559b03e0c694015ba5afdce8724e3f86c99
KarnaVidyaRatnaSri <137883417+KarnaVidyaRatnaSri@users.noreply.github.com>
2023-07-29 15:32:27 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,11 @@
+// import logo from './logo.svg';
+import Display from './Components/Display'
+
+function App() {
+  return (
+    <div className="App">
+      <Display/>
+    </div>
+  );
+}
+export default App;
